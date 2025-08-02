7cd4486e9dfc83c515b78a10ce1245e9720a3697
Camilo Coelho Gomes <camilo.coelho.gomes@gmail.com>
2025-08-02 12:13:53 -0300

    adição da capacidade de circuit break

--- src/main/java/com/backend/figth/client/PaymentProcessorClient.java
+++ src/main/java/com/backend/figth/client/PaymentProcessorClient.java
@@ -2,6 +2,7 @@ package com.backend.figth.client;
 
 import com.backend.figth.dto.PaymentProcessorRequestDTO;
 import com.backend.figth.dto.PaymentProcessorResponseDTO;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,6 +10,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 @FeignClient(name = "payment-processor")
 public interface PaymentProcessorClient {
 
+  @Retry(name = "payment-processor", fallbackMethod = "processPaymentFallback")
   @PostMapping("/payments")
   PaymentProcessorResponseDTO processPayment(@RequestBody PaymentProcessorRequestDTO request);
+
+  // Método fallback que será chamado se retry estiver desabilitado ou falhar
+  default PaymentProcessorResponseDTO processPaymentFallback(PaymentProcessorRequestDTO request, Exception e) {
+    // Retorna uma resposta de erro padrão
+    return new PaymentProcessorResponseDTO("Payment processor unavailable", "FAILED");
+  }
 }
