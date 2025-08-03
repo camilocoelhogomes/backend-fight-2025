91e6d02ef7b7ede375e14a3121dee834639169ac
Camilo Coelho Gomes <camilo.coelho.gomes@gmail.com>
2025-08-03 14:43:52 -0300

    ajuste na classe

--- src/main/java/com/backend/figth/dto/PaymentSummaryQueryDTO.java
+++ src/main/java/com/backend/figth/dto/PaymentSummaryQueryDTO.java
@@ -2,17 +2,8 @@ package com.backend.figth.dto;
 
 import java.math.BigDecimal;
 
-import lombok.Getter;
-
-@Getter
-public class PaymentSummaryQueryDTO {
-    private Character paymentService;
-    private Long totalRequests;
-    private BigDecimal totalAmount;
-
-    public PaymentSummaryQueryDTO(Character paymentService, Long totalRequests, BigDecimal totalAmount) {
-        this.paymentService = paymentService;
-        this.totalRequests = totalRequests;
-        this.totalAmount = totalAmount;
-    }
+public record PaymentSummaryQueryDTO(
+        Character paymentService,
+        Long totalRequests,
+        BigDecimal totalAmount) {
 }
